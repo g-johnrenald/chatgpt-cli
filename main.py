f116cd79687f124b747608989457d20a6d02f49d
John Garcines <johnrenald2013@gmail.com>
2023-03-07 21:09:33 +0800

    Read txt file for system_message

--- main.py
+++ main.py
@@ -3,34 +3,22 @@ import time
 from rich import print
 from dotenv import load_dotenv
 from tqdm import tqdm
-
 from chat_gpt_wrapper import ChatGpt
 from langchain_wrapper import LangChainGpt
-
-
-def show_waiting_indicator(func):
-    def wrapper(*args, **kwargs):
-        progress_bar = tqdm(total=100, desc="Waiting for response...",
-                            unit="%")
-        for i in range(100):
-            progress_bar.update(1)
-            time.sleep(0.05)
-        progress_bar.close()
-        result = func(*args, **kwargs)
-        return result
-
-    return wrapper
-
 
 if __name__ == '__main__':
     # Load environment variables
     load_dotenv()
     token = os.getenv("OPENAI_API_KEY")
 
-    initial_prompt = "You are a helpful assistant that is very good at problem solving who thinks step by step."
+    try:
+        with open('system_message.txt', 'r') as f:
+            system_message = f.read()
+    except FileNotFoundError:
+        print('File not found')
 
-    chat_gpt = ChatGpt(key=token, temperature=1, initial_prompt=initial_prompt)
-    langchain_gpt = LangChainGpt(system_message=initial_prompt)
+    chat_gpt = ChatGpt(key=token, temperature=1, initial_prompt=system_message)
+    langchain_gpt = LangChainGpt(system_message=system_message)
 
     command = input("Use langchain? (y/n), q to quit: ")
     while True:
@@ -47,7 +35,7 @@ if __name__ == '__main__':
             break
 
         if user_prompt == "o":
-            command = input("Use langchain? (y/n): ")
+            command = input("Use langchain? (y/n), q to quit: ")
             continue
 
         if command == "y":
@@ -55,7 +43,6 @@ if __name__ == '__main__':
             langchain_gpt.predict(user_prompt)
             print("\n")
         elif command == "n":
-            # print(user_prompt.strip())
             answer = chat_gpt.predict(user_prompt)
             print(f"\nGPT: {answer}\n")
         elif command == "q":
