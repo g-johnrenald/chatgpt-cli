4fed0b9d954af6a1e1df6b269b99188bb414dd2a
John Garcines <john.garcines@linkforce.com.au>
2023-03-08 10:23:00 +0800

    Add memory to streaming llm and update langchain

--- langchain_wrapper.py
+++ langchain_wrapper.py
@@ -1,18 +1,27 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    HumanMessage,
-    SystemMessage
-)
+from langchain import ConversationChain
 from langchain.callbacks.base import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate
+)
 
 
 class LangChainGpt:
     def __init__(self, system_message):
-        self.sm = SystemMessage(content=system_message)
-        self.chat = ChatOpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-                               verbose=True, temperature=0)
+        self.prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(system_message),
+            MessagesPlaceholder(variable_name="history"),
+            HumanMessagePromptTemplate.from_template("{input}")
+        ])
+        self.llm = ChatOpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+                              verbose=True, temperature=0)
+        self.memory = ConversationBufferMemory(return_messages=True)
+        self.conversation = ConversationChain(memory=self.memory, prompt=self.prompt, llm=self.llm)
 
     def predict(self, user_prompt):
-        hm = HumanMessage(content=user_prompt)
-        return self.chat([self.sm, hm])
+        return self.conversation.predict(input=user_prompt)
